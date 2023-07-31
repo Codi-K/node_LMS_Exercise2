2c1ff7ad51d83bb306eccd75d5cad4b4ab484cb4
Codi-K <codikader7@gmail.com>
2023-07-31 15:41:29 +0200

    Exercise 2 done

---
+++ index.js
@@ -0,0 +1,13 @@
+const Person = require('./person')
+
+const person1 = new Person("Keeno", "Fourie", "Keeno@gmail.com");
+
+console.log(person1);
+
+const person2 = new Person("Codi", "Kader", "Codi@gmail.com");
+
+console.log(person2);
+
+const person3 = new Person("Taflynne", "Petersen", "Taflynne@gmail.com");
+
+console.log(person3);
---
+++ person.js
@@ -0,0 +1,11 @@
+
+
+function Person(firstName, lastName, emailAddress){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.emailAddress = emailAddress
+
+}
+ 
+module.exports = Person
+
